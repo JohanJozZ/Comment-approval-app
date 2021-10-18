b478e6532b1e35bfa7878fee4bbbeb5585dd073a
JohanJozZ <jjgomez1998@hotmail.com>
2021-10-18 15:24:27 -0400

    Added a new component to display approved comments

--- src/components/CardComment.jsx
+++ src/components/CardComment.jsx
@@ -5,8 +5,8 @@ const CardComment = (props) => {
     <div className="card">
       <div className="content">{props.children}</div>
       <div className="ui buttons two">
-        <div className="ui basic green button">Approve</div>
-        <div className="ui basic red button">Reject</div>
+        <div className="ui green button">Approve</div>
+        <div className="ui red button">Reject</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,48 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './components/CommentDetail';
 import CardComment from './components/CardComment';
+import CommentFeed from './components/CommentFeed';
 
 class App extends React.Component {
   state = {
-    comments: {
-      author: 'Luis',
-      profile: 'customer-1.jpg',
-      text: 'Nice!',
-      timeAgo: 'Today at 3:30pm',
-    },
+    approvedComments: [
+      {
+        author: 'Luis',
+        profile: 'customer-1.jpg',
+        text: 'Nice!',
+        timeAgo: 'Today at 3:30pm',
+      },
+    ],
   };
 
   componentDidMount() {
-    console.log(this);
+    console.log(this.state);
   }
 
   render() {
     return (
-      <div className="ui container comments cards">
-        <CardComment>
-          <CommentDetail
-            author="Luis"
-            profile="customer-1.jpg"
-            text="Nice!"
-            timeAgo="Today at 3:30pm"
-          />
-        </CardComment>
-        <CardComment>
-          <CommentDetail
-            author="Sara"
-            profile="customer-2.jpg"
-            text="Please keep up with this"
-            timeAgo="Yesterday at 1:30pm"
-          />
-        </CardComment>
-        <CardComment>
-          <CommentDetail
-            author="Pedro"
-            profile="customer-3.jpg"
-            text="Comment number 3"
-            timeAgo="Yesterday at 8:30am"
-          />
-        </CardComment>
+      <div className="ui segment">
+        <div className="ui two column grid">
+          <div className="ui container comments cards column">
+            <CardComment>
+              <CommentDetail
+                author="Luis"
+                profile="customer-1.jpg"
+                text="Nice!"
+                timeAgo="Today at 3:30pm"
+              />
+            </CardComment>
+            <CardComment>
+              <CommentDetail
+                author="Sara"
+                profile="customer-2.jpg"
+                text="Please keep up with this"
+                timeAgo="Yesterday at 1:30pm"
+              />
+            </CardComment>
+            <CardComment>
+              <CommentDetail
+                author="Pedro"
+                profile="customer-3.jpg"
+                text="Comment number 3"
+                timeAgo="Yesterday at 8:30am"
+              />
+            </CardComment>
+          </div>
+          <div className="column">
+            <CommentFeed>
+              <CommentDetail
+                author={this.state.approvedComments[0].author}
+                profile="customer-3.jpg"
+              />
+            </CommentFeed>
+          </div>
+        </div>
       </div>
     );
   }
